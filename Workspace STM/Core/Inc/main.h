32a6b20f9f6bc65c8b6a86dc2e5639ee50f4cdc5
jang130 <50463317+jang130@users.noreply.github.com>
2023-12-04 18:37:56 +0100

    Added pins to display and i2c

--- Workspace STM/Core/Inc/main.h
+++ Workspace STM/Core/Inc/main.h
@@ -66,6 +66,10 @@ void Error_Handler(void);
 #define USART_RX_GPIO_Port GPIOA
 #define LD2_Pin GPIO_PIN_5
 #define LD2_GPIO_Port GPIOA
+#define DATA_Pin GPIO_PIN_0
+#define DATA_GPIO_Port GPIOB
+#define CLK_Pin GPIO_PIN_7
+#define CLK_GPIO_Port GPIOC
 #define TMS_Pin GPIO_PIN_13
 #define TMS_GPIO_Port GPIOA
 #define TCK_Pin GPIO_PIN_14
